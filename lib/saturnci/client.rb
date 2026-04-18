75bf501ab694cb3ae7d52c24ad4079083ffd560e
Jason Swett <jason@benfranklinlabs.com>
2026-04-18 15:13:59 -0400

    Extract authentication check path to a named constant.

--- lib/saturnci/client.rb
+++ lib/saturnci/client.rb
@@ -5,6 +5,8 @@ require 'uri'
 
 module SaturnCI
   class Client
+    AUTHENTICATION_CHECK_PATH = '/api/v1/test_suite_runs'
+
     def initialize(user_id: nil, api_token: nil, base_url: 'https://app.saturnci.com')
       @user_id = user_id
       @api_token = api_token
@@ -17,7 +19,7 @@ module SaturnCI
         return false
       end
 
-      uri = URI("#{@base_url}/api/v1/test_suite_runs")
+      uri = URI("#{@base_url}#{AUTHENTICATION_CHECK_PATH}")
       req = Net::HTTP::Get.new(uri)
       req.basic_auth(@user_id, @api_token)
       response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http| http.request(req) }
