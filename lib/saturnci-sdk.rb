77c20297182be864045e7c81b72fe00ad1b7f5b0
Jason Swett <jason@benfranklinlabs.com>
2026-04-25 17:33:38 -0400

    Remove SaturnCI::Build class.

--- lib/saturnci-sdk.rb
+++ lib/saturnci-sdk.rb
@@ -3,7 +3,6 @@
 require_relative 'saturnci/version'
 require_relative 'saturnci/credentials'
 require_relative 'saturnci/client'
-require_relative 'saturnci/build'
 require_relative 'saturnci/container_image_build'
 require_relative 'saturnci/job_run'
 require_relative 'saturnci/test_suite_run'
