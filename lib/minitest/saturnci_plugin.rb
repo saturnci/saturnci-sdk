bd8e55ccc1ad796bb07c24f559fb551f1d1e519b
Jason Swett <jason@benfranklinlabs.com>
2026-04-28 22:04:37 -0400

    Ensure the Minitest output directory exists before opening the file.

--- lib/minitest/saturnci_plugin.rb
+++ lib/minitest/saturnci_plugin.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'fileutils'
 require 'saturnci/minitest_reporter'
 
 module Minitest
@@ -7,6 +8,7 @@ module Minitest
     output_path = ENV.fetch('SATURNCI_MINITEST_OUTPUT_PATH', nil)
     return unless output_path
 
+    FileUtils.mkdir_p(File.dirname(output_path))
     Minitest.reporter << SaturnCI::MinitestReporter.new(File.open(output_path, 'w'))
   end
 end
