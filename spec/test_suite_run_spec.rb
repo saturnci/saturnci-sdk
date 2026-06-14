2478387d698991bdf391621358d3a1eccc39538d
Jason Swett <jason@benfranklinlabs.com>
2026-06-14 08:58:05 -0400

    Accept parent_job_run_id when creating a test suite run.

--- spec/test_suite_run_spec.rb
+++ spec/test_suite_run_spec.rb
@@ -61,6 +61,25 @@ describe SaturnCI::TestSuiteRun do
       expect(stub).to have_been_requested
     end
 
+    it 'forwards parent_job_run_id to the API when given' do
+      stub = stub_request(:post, 'https://app.saturnci.com/api/v1/test_suite_runs')
+             .with(body: hash_including('parent_job_run_id' => 'parent123'))
+             .to_return(status: 201, body: '{"id": "abc123"}')
+
+      client = SaturnCI::Client.new(TestHelpers.credentials)
+      SaturnCI::TestSuiteRun.create(
+        client: client,
+        repository: 'saturnci/words',
+        branch_name: 'main',
+        commit_hash: 'abc123',
+        commit_message: 'Add feature',
+        author_name: 'Jason',
+        parent_job_run_id: 'parent123'
+      )
+
+      expect(stub).to have_been_requested
+    end
+
     it 'omits command from the request body when not given' do
       stub = stub_request(:post, 'https://app.saturnci.com/api/v1/test_suite_runs')
              .with { |request| !request.body.include?('command') }
@@ -82,6 +101,24 @@ describe SaturnCI::TestSuiteRun do
     it 'omits task_adapter_name from the request body when not given' do
       stub = stub_request(:post, 'https://app.saturnci.com/api/v1/test_suite_runs')
              .with { |request| !request.body.include?('task_adapter_name') }
+             .to_return(status: 201, body: '{"id": "abc123"}')
+
+      client = SaturnCI::Client.new(TestHelpers.credentials)
+      SaturnCI::TestSuiteRun.create(
+        client: client,
+        repository: 'saturnci/book_tracker',
+        branch_name: 'main',
+        commit_hash: 'abc123',
+        commit_message: 'Add feature',
+        author_name: 'Jason'
+      )
+
+      expect(stub).to have_been_requested
+    end
+
+    it 'omits parent_job_run_id from the request body when not given' do
+      stub = stub_request(:post, 'https://app.saturnci.com/api/v1/test_suite_runs')
+             .with { |request| !request.body.include?('parent_job_run_id') }
              .to_return(status: 201, body: '{"id": "abc123"}')
 
       client = SaturnCI::Client.new(TestHelpers.credentials)
